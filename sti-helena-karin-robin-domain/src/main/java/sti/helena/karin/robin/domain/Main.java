5df3d74af0ac789d8bcea90ad0167339ff7a92ae
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-02-01 10:37:40 +0100

    added logger

--- sti-helena-karin-robin-domain/src/main/java/sti/helena/karin/robin/domain/Main.java
+++ sti-helena-karin-robin-domain/src/main/java/sti/helena/karin/robin/domain/Main.java
@@ -1,7 +1,22 @@
 package sti.helena.karin.robin.domain;
 
+import sti.helena.karin.robin.service.Logg;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        try{
+            Logg mylogg = new Logg("logg.txt");
+            mylogg.logger.setLevel(Level.ALL);
+        } catch (IOException e) {
+        }
+
+
     }
+
+
 }
