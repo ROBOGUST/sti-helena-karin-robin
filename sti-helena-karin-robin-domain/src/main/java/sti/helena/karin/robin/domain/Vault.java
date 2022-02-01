39090c1755febb2f756293eb7fa72b04cfc8274c
HelenaOstlund <92034375+HelenaOstlund@users.noreply.github.com>
2022-02-01 15:18:52 +0100

    added students

--- sti-helena-karin-robin-domain/src/main/java/sti/helena/karin/robin/domain/Vault.java
+++ sti-helena-karin-robin-domain/src/main/java/sti/helena/karin/robin/domain/Vault.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 
 public class Vault {
 
-    Student student1= new Student("Adam", "Svensson", "19730412-4433");
-    Student student2= new Student("Klara", "Olsson", "19920327-3445");
-    Student student3= new Student("Sven", "Svensson", "19730232-4556");
+
+
+    static Student student1= new Student("Adam", "Svensson", 197304124433L);
+    static Student student2= new Student("Klara", "Olsson", 199203273445L);
+    static Student student3= new Student("Sven", "Svensson", 197302324556L);
 
     Course kurs1= new Course("Java A", 1, 1, 160, 20);
     Course kurs2= new Course("Databas", 2, 2, 200, 25);
     Course kurs3= new Course("Java B", 3, 1, 200, 25);
 
     Teacher teacher1 = new Teacher("Gurran", "Karlsson", 195604302354L);
-    Teacher teacher2 = new Teacher("Gittan", "Gittanson", 197303126543L );
+    Teacher teacher2 = new Teacher("Gittan", "Gittanson", 197303126543L);
 
 
 
 
 
 
-    ArrayList<String> Students = new ArrayList<>();
+
 
 
 }
